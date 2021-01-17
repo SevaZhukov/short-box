41c59384ac3ec4864623438505d364b7f03ff632
szhukov <s.zhukov@cft.ru>
2021-01-17 23:36:27 +0700

    сделал дроп стека

--- app/src/main/kotlin/com/siberiandroid/shortbox/navigation/GlobalNavigationGraph.kt
+++ app/src/main/kotlin/com/siberiandroid/shortbox/navigation/GlobalNavigationGraph.kt
@@ -6,12 +6,16 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigate
+import androidx.navigation.compose.popUpTo
 import androidx.navigation.compose.rememberNavController
 import com.siberiandroid.shortbox.feature.main.Main
 import com.siberiandroid.shortbox.feature.onboarding.presentation.Onboarding
 import com.siberiandroid.shortbox.feature.splash.presentation.Splash
+import com.siberiandroid.shortbox.navigation.GlobalDestinations.HOST
 
 object GlobalDestinations {
+
+	const val HOST = "host"
 
 	const val SPLASH = "splash"
 	const val MAIN = "main"
@@ -20,8 +24,16 @@ object GlobalDestinations {
 
 class GlobalActions(navController: NavController) {
 
-	val navigateToMain = { navController.navigate(GlobalDestinations.MAIN) }
-	val navigateToOnboarding = { navController.navigate(GlobalDestinations.ONBOARDING) }
+	val navigateToMain = {
+		navController.navigate(GlobalDestinations.MAIN) {
+			popUpTo(HOST) { inclusive = true }
+		}
+	}
+	val navigateToOnboarding = {
+		navController.navigate(GlobalDestinations.ONBOARDING) {
+			popUpTo(HOST) { inclusive = true }
+		}
+	}
 }
 
 @Composable
@@ -30,7 +42,7 @@ fun GlobalNavigationGraph() {
 
 	val actions = remember(navController) { GlobalActions(navController) }
 
-	NavHost(navController, GlobalDestinations.SPLASH) {
+	NavHost(navController, GlobalDestinations.SPLASH, GlobalDestinations.HOST) {
 		composable(GlobalDestinations.SPLASH) {
 			Splash(actions.navigateToMain, actions.navigateToOnboarding)
 		}

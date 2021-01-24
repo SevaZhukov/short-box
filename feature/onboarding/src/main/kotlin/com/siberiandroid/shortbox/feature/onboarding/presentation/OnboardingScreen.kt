ca7c5b40366dd11e3c554e333aae3c6e5b8594cf
szhukov <s.zhukov@cft.ru>
2021-01-25 00:46:11 +0700

    переключение между слайдами

--- feature/onboarding/src/main/kotlin/com/siberiandroid/shortbox/feature/onboarding/presentation/OnboardingScreen.kt
+++ feature/onboarding/src/main/kotlin/com/siberiandroid/shortbox/feature/onboarding/presentation/OnboardingScreen.kt
@@ -4,9 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.preferredHeight
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.Button
@@ -14,7 +13,6 @@ import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FiberManualRecord
@@ -22,6 +20,8 @@ import androidx.compose.material.icons.outlined.FiberManualRecord
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.savedinstancestate.savedInstanceState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -41,61 +41,45 @@ fun OnboardingScreen(
 
 	val texts = listOf("текст 1", "текст 2", "текст 3", "текст 4")
 
+	var currentSlideIndex by savedInstanceState { 0 }
+
 	ShortboxTheme {
-		/*Text(text = "Onboarding")
-		Button({ viewModel.passOnboarding() }) {
-			Text("understand")
-		}*/
-		Surface(modifier = Modifier.fillMaxSize()) {
-			Scaffold(
-				bodyContent = {
-					Column {
-						Text(
-							text = "Слайд 1",
-							style = MaterialTheme.typography.subtitle1
-						)
-						Spacer(modifier = Modifier.height(24.dp))
-						Text("Текст 1")
-					}
-				},
-				bottomBar = {
-					Surface(
-//						elevation = 3.dp
-					) {
-						Row(
-							modifier = Modifier.padding(horizontal = 16.dp, vertical = 16.dp)
-						) {
-							OutlinedButton(
-								modifier = Modifier.weight(1f),
-								onClick = {}
-							) {
-								Text("Назад")
-							}
-							Spacer(modifier = Modifier.width(16.dp))
-							PageIndicator(
-								pagesCount = 4,
-								currentPageIndex = 0
-							)
-							Button(
-								modifier = Modifier.weight(1f),
-								onClick = {}
-							) {
-								Text("Дальше")
-							}
-						}
-					}
+		Scaffold(
+			modifier = Modifier.fillMaxWidth(),
+			bodyContent = {
+				SlideBody(text = texts[currentSlideIndex])
+			},
+			bottomBar = {
+				Column(modifier = Modifier.fillMaxWidth()) {
+					SlideIndicator(
+						pagesCount = texts.size,
+						currentPageIndex = currentSlideIndex
+					)
+					SlideButtons(
+						onPreviousPressed = { currentSlideIndex-- },
+						onNextPressed = { currentSlideIndex++ }
+					)
 				}
-			)
-		}
+			}
+		)
 	}
 }
 
+@Composable
+private fun SlideBody(text: String) {
+	Column {
+		Text(
+			text = text,
+			style = MaterialTheme.typography.subtitle1
+		)
+	}
+}
 
 @Composable
-private fun PageIndicator(pagesCount: Int, currentPageIndex: Int, modifier: Modifier = Modifier) {
+private fun SlideIndicator(pagesCount: Int, currentPageIndex: Int) {
 	Row(
-		modifier = modifier
-			.wrapContentWidth(align = Alignment.CenterHorizontally)
+		modifier = Modifier.fillMaxWidth()
+			.padding(horizontal = 16.dp, vertical = 16.dp)
 	) {
 		for (pageIndex in 0 until pagesCount) {
 			val asset = if (currentPageIndex == pageIndex) {
@@ -107,6 +91,28 @@ private fun PageIndicator(pagesCount: Int, currentPageIndex: Int, modifier: Modi
 				imageVector = asset,
 				tint = MaterialTheme.colors.primary
 			)
+		}
+	}
+}
+
+@Composable
+private fun SlideButtons(
+	onPreviousPressed: () -> Unit,
+	onNextPressed: () -> Unit
+) {
+	Row(modifier = Modifier.padding(horizontal = 16.dp, vertical = 16.dp)) {
+		OutlinedButton(
+			modifier = Modifier.weight(1f),
+			onClick = { onPreviousPressed() }
+		) {
+			Text("Назад")
+		}
+		Spacer(modifier = Modifier.width(16.dp))
+		Button(
+			modifier = Modifier.weight(1f),
+			onClick = { onNextPressed() }
+		) {
+			Text("Дальше")
 		}
 	}
 }
